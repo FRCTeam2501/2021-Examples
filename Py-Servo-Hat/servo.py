1483afe4ab8ebf37f8662306fbbdaf2cfc01512e
TylerSeiford <50031864+TylerSeiford@users.noreply.github.com>
2020-12-08 16:19:21 -0600

    Working again

--- Py-Servo-Hat/servo.py
+++ Py-Servo-Hat/servo.py
@@ -6,10 +6,13 @@ kit = ServoKit(channels=16)
 while True:
     print("1")
     kit.continuous_servo[0].throttle = 1
-    time.sleep(5)
+    kit.continuous_servo[1].throttle = -1
+    time.sleep(1)
     print("-1")
     kit.continuous_servo[0].throttle = -1
-    time.sleep(5)
+    kit.continuous_servo[1].throttle = 1
+    time.sleep(1)
     print("0")
+    kit.continuous_servo[0].throttle = 0
     kit.continuous_servo[1].throttle = 0
-    time.sleep(5)
+    time.sleep(1)
