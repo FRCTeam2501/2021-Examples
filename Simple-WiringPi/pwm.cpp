72e6e9a39327129664088ec4e849a7ae51d24028
TylerSeiford <50031864+TylerSeiford@users.noreply.github.com>
2021-01-09 09:50:25 -0600

    Some updates for pwm

--- Simple-WiringPi/pwm.cpp
+++ Simple-WiringPi/pwm.cpp
@@ -3,23 +3,23 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello world!\n";
-    wiringPiSetup();
-    
-    softPwmCreate(0, 0, 100);
-    std::cout << "Setup done.\n";
+	std::cout << "Hello world!\n";
+	wiringPiSetup();
+	
+	softPwmCreate(0, 0, 100);
+	std::cout << "Setup done.\n";
 
-    while(true) {
-        softPwmWrite(0, 75);
-        std::cout << "75%\n";
-        delay(5000);
-        softPwmWrite(0, 50);
-        std::cout << "50%\n";
-        delay(5000);
-        softPwmWrite(0, 25);
-        std::cout << "25%\n";
-        delay(5000);
-    }
+	while(true) {
+		softPwmWrite(0, 75);
+		std::cout << "75%\n";
+		delay(5000);
+		softPwmWrite(0, 50);
+		std::cout << "50%\n";
+		delay(5000);
+		softPwmWrite(0, 25);
+		std::cout << "25%\n";
+		delay(5000);
+	}
 
-    return 0;
+	return 0;
 }
