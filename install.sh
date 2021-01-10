5dababe7c97dc70d1ecc07c648e50d23a34949a5
TylerSeiford <50031864+TylerSeiford@users.noreply.github.com>
2021-01-10 12:09:34 -0600

    Added ARGB lib to install.sh

--- install.sh
+++ install.sh
@@ -34,3 +34,19 @@ git clone https://github.com/vanvught/rpidmx512.git
 cd rpidmx512/lib-pca9685/
 echo -e '\033[1;36m - Building library\033[0m'
 make -f Makefile.Linux "DEF=-DRASPPI"
+
+
+# Download and extract RPI_WS281X library
+echo -e '\033[1;36m***** INSTALLING RPI_WS281X *****\033[0m'
+cd /temp
+echo -e '\033[1;36m - Getting library\033[0m'
+git clone https://github.com/jgarff/rpi_ws281x.git
+cd rpi_ws281x
+mkdir build
+cd build
+echo -e '\033[1;36m - Configuring library\033[0m'
+cmake -D BUILD_SHARED=OFF -D BUILD_TEST=ON ..
+echo -e '\033[1;36m - Building library\033[0m'
+cmake --build .
+echo -e '\033[1;36m - Installing library\033[0m'
+sudo make install
