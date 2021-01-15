0a638ea8bca5fa6716bc4fe42f3df4d7f6e71ede
TylerSeiford <50031864+TylerSeiford@users.noreply.github.com>
2021-01-15 16:29:57 -0600

    rpidmx512 lib is now installed properly

--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Defining variables
-LIB_ROOT=~
-LIB_DIR=lib2501
-
-
-
 # Download and extract BCM2835 library
 echo -e '\033[1;36m***** INSTALLING BCM2835 *****\033[0m'
 cd /tmp
@@ -26,19 +20,20 @@ sudo make install
 
 # Download and extract RPIDMX512 library
 echo -e '\033[1;36m***** INSTALLING RPIDMX512 *****\033[0m'
-cd ${LIB_ROOT}
-mkdir ${LIB_DIR}
-cd ${LIB_DIR}
+cd /tmp
 echo -e '\033[1;36m - Getting library\033[0m'
 git clone https://github.com/vanvught/rpidmx512.git
 cd rpidmx512/lib-pca9685/
 echo -e '\033[1;36m - Building library\033[0m'
 make -f Makefile.Linux "DEF=-DRASPPI"
+echo -e '\033[1;36m - Installing library\033[0m'
+sudo cp include/* /usr/local/include
+sudo cp lib_linux/libpca9685.a /usr/local/lib/
 
 
 # Download and extract RPI_WS281X library
 echo -e '\033[1;36m***** INSTALLING RPI_WS281X *****\033[0m'
-cd /temp
+cd /tmp
 echo -e '\033[1;36m - Getting library\033[0m'
 git clone https://github.com/jgarff/rpi_ws281x.git
 cd rpi_ws281x
