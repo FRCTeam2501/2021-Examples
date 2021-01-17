24f419faff9eac9290c78485294dadd960369798
TylerSeiford <50031864+TylerSeiford@users.noreply.github.com>
2021-01-17 16:27:53 -0600

    Started working on RGB-SL code

--- ARGB/main.cpp
+++ ARGB/main.cpp
@@ -1,34 +1,4 @@
-/*
-* newtest.c
-*
-* Copyright (c) 2014 Jeremy Garff <jer @ jers.net>
-*
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without modification, are permitted
-* provided that the following conditions are met:
-*
-*     1.  Redistributions of source code must retain the above copyright notice, this list of
-*         conditions and the following disclaimer.
-*     2.  Redistributions in binary form must reproduce the above copyright notice, this list
-*         of conditions and the following disclaimer in the documentation and/or other materials
-*         provided with the distribution.
-*     3.  Neither the name of the owner nor the names of its contributors may be used to endorse
-*         or promote products derived from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
-* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE
-* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-* OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-*/
-
-
-static char VERSION[] = "XX.YY.ZZ";
+#include <iostream>
 
 #include <stdint.h>
 #include <stdio.h>
@@ -104,14 +74,14 @@ void matrix_clear(void) {
 
 int dotspos[] = { 0, 1, 2, 3, 4, 5, 6, 7 };
 ws2811_led_t dotcolors[] = {
-	0x00800000,  // red
-	0x00804000,  // orange
-	0x00808000,  // yellow
-	0x00008000,  // green
-	0x00008020,  // lightblue
-	0x00000080,  // blue
-	0x00400040,  // purple
-	0x00800040,  // pink
+	0x00400000,  // red
+	0x00402000,  // orange
+	0x00404000,  // yellow
+	0x00004000,  // green
+	0x00004010,  // lightblue
+	0x00000040,  // blue
+	0x00200020,  // purple
+	0x00400020,  // pink
 };
 
 void matrix_bottom(void) {
@@ -128,6 +98,7 @@ void matrix_bottom(void) {
 static void ctrl_c_handler(int signum) {
 	(void) (signum);
 	running = 0;
+	std::cout << "\nStopped\n";
 }
 
 static void setup_handlers(void) {
@@ -136,113 +107,6 @@ static void setup_handlers(void) {
 
 	sigaction(SIGINT, &sa, NULL);
 	sigaction(SIGTERM, &sa, NULL);
-}
-
-
-void parseargs(int argc, char **argv, ws2811_t *ws2811) {
-	int index;
-	int c;
-
-	static struct option longopts[] = {
-		{"help", no_argument, 0, 'h'},
-		{"dma", required_argument, 0, 'd'},
-		{"gpio", required_argument, 0, 'g'},
-		{"invert", no_argument, 0, 'i'},
-		{"strip", required_argument, 0, 's'},
-		{"height", required_argument, 0, 'y'},
-		{"width", required_argument, 0, 'x'},
-		{"version", no_argument, 0, 'v'},
-		{0, 0, 0, 0}
-	};
-
-	while (1) {
-		index = 0;
-		c = getopt_long(argc, argv, "cd:g:his:vx:y:", longopts, &index);
-
-		if (c == -1)
-			break;
-
-		switch (c)
-		{
-		case 0:
-			/* handle flag options (array's 3rd field non-0) */
-			break;
-
-		case 'h':
-			fprintf(stderr, "%s version %s\n", argv[0], VERSION);
-			fprintf(stderr, "Usage: %s \n"
-				"-h (--help)    - this information\n"
-				"-s (--strip)   - strip type - rgb, grb, gbr, rgbw\n"
-				"-d (--dma)     - dma channel to use (default 10)\n"
-				"-i (--invert)  - invert pin output (pulse LOW)\n"
-				"-v (--version) - version information\n"
-				, argv[0]);
-			exit(-1);
-
-		case 'D':
-			break;
-
-		case 'i':
-			ws2811->channel[0].invert=1;
-			break;
-
-		case 'd':
-			if (optarg) {
-				int dma = atoi(optarg);
-				if (dma < 14) {
-					ws2811->dmanum = dma;
-				} else {
-					printf ("invalid dma %d\n", dma);
-					exit (-1);
-				}
-			}
-			break;
-
-		case 's':
-			if (optarg) {
-				if (!strncasecmp("rgb", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_RGB;
-				}
-				else if (!strncasecmp("rbg", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_RBG;
-				}
-				else if (!strncasecmp("grb", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_GRB;
-				}
-				else if (!strncasecmp("gbr", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_GBR;
-				}
-				else if (!strncasecmp("brg", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_BRG;
-				}
-				else if (!strncasecmp("bgr", optarg, 4)) {
-					ws2811->channel[0].strip_type = WS2811_STRIP_BGR;
-				}
-				else if (!strncasecmp("rgbw", optarg, 4)) {
-					ws2811->channel[0].strip_type = SK6812_STRIP_RGBW;
-				}
-				else if (!strncasecmp("grbw", optarg, 4)) {
-					ws2811->channel[0].strip_type = SK6812_STRIP_GRBW;
-				}
-				else {
-					printf ("invalid strip %s\n", optarg);
-					exit (-1);
-				}
-			}
-			break;
-
-		case 'v':
-			fprintf(stderr, "%s version %s\n", argv[0], VERSION);
-			exit(-1);
-
-		case '?':
-			/* getopt_long already reported error? */
-			exit(-1);
-
-		default:
-			exit(-1);
-		}
-	}
 }
 
 
@@ -261,10 +125,6 @@ int main(int argc, char *argv[]) {
 
 	ws2811_return_t ret;
 
-	sprintf(VERSION, "%d.%d.%d", 1, 1, 0);
-
-	parseargs(argc, argv, &ledstring);
-
 	matrix = (ws2811_led_t *) malloc(sizeof(ws2811_led_t) * width * height);
 
 	setup_handlers();
@@ -274,6 +134,7 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "ws2811_init failed: %s\n", ws2811_get_return_t_str(ret));
 		return ret;
 	}
+	std::cout << "Started\n";
 
 	while (running)
 	{
