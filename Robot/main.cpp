e9b0e6115761b65e198b8cd14d9ed8b3f74bc98c
TylerSeiford <50031864+TylerSeiford@users.noreply.github.com>
2021-01-19 13:08:45 -0600

    Started working toward abstraction

--- Robot/main.cpp
+++ Robot/main.cpp
@@ -11,7 +11,7 @@
 
 
 static ARGB *rgb;
-static bool running = false;
+static bool running = true;
 
 static void ctrl_c_handler(int signum) {
 	(void) (signum);
@@ -27,31 +27,56 @@ static void setup_handlers(void) {
 	sigaction(SIGTERM, &sa, NULL);
 }
 
-int main() {
-	setup_handlers();
 
-	Joystick *stick = new Joystick(0U);
+Joystick *stick;
+ServoHat *hat;
+SpeedController *lf, *lr, *rf, *rr, *shooter;
+DifferentialDrive *drive;
+bool startWasPressed = false;
+bool aWasPressed = false;
+
+void Setup() {
+	stick = new Joystick(0U);
 	if(!stick->IsOpen()) {
-		return -255;
+		exit(-255);
 	}
 
-	ServoHat *hat = new ServoHat();
-	SpeedController *lf = new SpeedController(hat, 0U),
-					*lr = new SpeedController(hat, 2U),
-					*rf = new SpeedController(hat, 1U),
-					*rr = new SpeedController(hat, 3U),
-					*shooter = new SpeedController(hat, 4U);
-	DifferentialDrive *drive = new DifferentialDrive(lf, lr, rf, rr);
+	hat = new ServoHat();
+	lf = new SpeedController(hat, 0U),
+	lr = new SpeedController(hat, 2U),
+	rf = new SpeedController(hat, 1U),
+	rr = new SpeedController(hat, 3U),
+	shooter = new SpeedController(hat, 4U);
+	drive = new DifferentialDrive(lf, lr, rf, rr);
 	drive->SetLeftInverted(true);
 	drive->SetRightInverted(true);
 
 	rgb = new ARGB();
 	rgb->Set(COLORS::BLUE);
+}
+
+void Enabled() {
+	// Deal with drive
+	drive->ArcadeDrive(stick->GetAxis(GAMEPAD::AXES::LY), stick->GetAxis(GAMEPAD::AXES::LX) * -1.0);
+
+	// Deal with shooter
+	if(stick->GetButton(GAMEPAD::BUTTONS::A) && !aWasPressed) {
+		shooter->Set(0.5);
+		std::cout << "Shooter: ON\n";
+		aWasPressed = true;
+	}
+	else if(!stick->GetButton(GAMEPAD::BUTTONS::A) && aWasPressed) {
+		shooter->Set(0.0);
+		std::cout << "Shooter: OFF\n";
+		aWasPressed = false;
+	}
+}
 
 
-	bool startWasPressed = false;
-	bool aWasPressed = false;
-	running = true;
+int main() {
+	setup_handlers();
+
+	Setup();
 	
 	while(running) {
 		// Sleep for 1ms
@@ -79,27 +104,9 @@ int main() {
 		else if(!stick->GetButton(GAMEPAD::BUTTONS::START) && startWasPressed) {
 			startWasPressed = false;
 		}
-
-
-		// Deal with drive
+		
 		if(hat->IsEnabled()) {
-			drive->ArcadeDrive(stick->GetAxis(GAMEPAD::AXES::LY), stick->GetAxis(GAMEPAD::AXES::LX) * -1.0);
-		}
-
-
-		// Deal with shooter
-		if(hat->IsEnabled()) {
-			// Button to run
-			if(stick->GetButton(GAMEPAD::BUTTONS::A) && !aWasPressed) {
-				shooter->Set(0.5);
-				std::cout << "Shooter: ON\n";
-				aWasPressed = true;
-			}
-			else if(!stick->GetButton(GAMEPAD::BUTTONS::A) && aWasPressed) {
-				shooter->Set(0.0);
-				std::cout << "Shooter: OFF\n";
-				aWasPressed = false;
-			}
+			Enabled();
 		}
 	}
 
